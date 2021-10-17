000e3241123a53f418c50c4b36e76fa553e24cff
gonzahce <carlosegonzax@gmail.com>
2021-10-17 11:36:30 -0400

    Core files

---
+++ core/core.php
@@ -0,0 +1,30 @@
+<?php
+
+#Constantes de conexión
+define ('DB_HOST','localhost');
+define ('DB_USER','root');
+define ('DB_PASS','');
+define ('DB_NAME','cms');
+
+#Constantes de la APP
+define('HTML_DIR','html/');
+define('APP_TITLE','Personal Portfolio');
+define('APP_COPY','Copyright &copy; ' . date('Y',time()) . ' Ocrend Software.');
+define('APP_URL','http://localhost/portfolio/');
+
+#Estructura
+
+require('core/models/class.Conexion.php');
+/*
+require('vendor/autoload.php'); 
+require('core/bin/functions/Encrypt.php');
+require('core/bin/functions/Users.php');
+require('core/bin/functions/Foros.php');
+require('core/bin/functions/Categorias.php');
+require('core/bin/functions/UrlAmigable.php');
+require('core/bin/functions/EmailTemplate.php');
+require('core/bin/functions/LostpassTemplate.php');
+
+$_users = Users();
+$_categorias = Categorias();
+$_foros = Foros(); */
